2afb2229fdbe81ec701f1bd408460039c519074c
Yannis Markonis <imarkonis@gmail.com>
2019-04-23 12:58:06 +0200

    more

--- R/somsp.R
+++ R/somsp.R
@@ -26,7 +26,9 @@
 #' 
 #' @export
 
-somspace <- function(x, ...){
+somsp <- function(x, ...) UseMethod("somsp") 
+
+somsp.default <- function(x, ...){
   x$som <- som(X = x$input_for_som, ...) 
   som_results <- data.table(id = x$coords$id, 
                             lat = x$coords$lat, 
@@ -38,5 +40,18 @@ somspace <- function(x, ...){
   som_results[, node_sd_lat := sd(lat), by = node]
   som_results[, node_sd_lon := sd(lon), by = node]
   som_results[, node_counts := .N, by = node]
-  return(list(summary = som_results, som = x$som))
+  out <- list(summary = som_results, som = x$som)
+  class(out) <- 'somsp'
+  return()
+}
+
+somsp.plot <- function(...){
+  colset_light_qual <- c("#8dd3c7",   "#fdb462", "#bebada", "#fb8072", 
+                         "#80b1d3",  "#b3de69", "#ffed6f","#bc80bd",      
+                         "#d9d9d9",  "#fccde5","#ccebc5", "#a1d6e2")
+  palette_light_qual <- colorRampPalette(colset_light_qual)
+  nnodes = max(som_data$summary$node)
+  my_col = palette_light_qual(nnodes)
+  plot(lat ~ lon, data = som_data$summary, pch = 15, col = my_col[node])
+  maps::map("world", add = TRUE)
 }
